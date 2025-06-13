76cd000fa30bcba7cf106453683f0539d73d0578
mrauthentik <umokeuchenna2020@gmail.com>
2025-06-13 13:13:24 +0100

    fix: adjust padding in LandingPage component for better layout consistency

--- imagefic/src/Components/LandingPage.tsx
+++ imagefic/src/Components/LandingPage.tsx
@@ -13,7 +13,7 @@ const LandingPage:React.FC = () => {
 <div className='z-20 flex justify-center pt-8 '>
     <img src={Zhentryx} alt="" className='w-52 md:w-80 h-24 md:h-32' />
 </div>
-            <div className='sub-section mt-64 relative text-white z-20 flex flex-col max-w-[700px] flex items-start justify-left leading-normal text-left max-w-4xl w-full px-4 md:px-12 lg:px-24  mt-16'
+            <div className='sub-section mt-64 relative text-white z-20 flex flex-col max-w-[700px] flex items-start justify-left leading-normal text-left max-w-4xl w-full px-4 md:px-12 lg:px-18  mt-16'
              >
                 <h2 className='text-5xl md:text-6xl font-castoro leading-tight mb-6 '>
                     GALLERY OF 
